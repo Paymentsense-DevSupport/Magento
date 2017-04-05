5e97fc8c3ab85dbffa26caf20ba8d5fdc59866ca
Paymentsense-OA <omar.akhmad@paymentsense.com>
2017-04-05 17:02:30 +0100

    Removed files added by mistake
    removed files added by mistake

--- app/code/local/Paymentsense/Payhosted/Block/Info/Payhosted.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Paymentsense_Payhosted_Block_Info_Payhosted extends Mage_Payment_Block_Info
-{
-	
-	
-}
--- app/code/local/Paymentsense/Payhosted/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Paymentsense_Payhosted_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
